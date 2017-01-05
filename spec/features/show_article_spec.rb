f1775fb63a8e66396993a1973275cbbd07527599
Marcus Allen <mk.allen2.0@gmail.com>
2017-01-05 18:22:59 +0000

    Update specs to verify sees edit-delete if authorized

--- spec/features/show_article_spec.rb
+++ spec/features/show_article_spec.rb
@@ -3,9 +3,9 @@ require "rails_helper"
 RSpec.feature "Showing an article" do
   
   before do
-    @steve = User.create(email: "steve@example.com", password: "password")
+    @Steve = User.create(email: "steve@example.com", password: "password")
     @Fred = User.create(email: "fred@example.com", password: "password")
-    @article = Article.create(title: "The first article", body: "Lorem ipsum dolar sit are, constru.", user: @steve)
+    @article = Article.create(title: "The first article", body: "Lorem ipsum dolar sit are, constru.", user: @Steve)
   end
   
   scenario "to non-signed in user hide the Edit and Delete buttons" do
@@ -32,6 +32,20 @@ RSpec.feature "Showing an article" do
     
     expect(page).not_to have_link("Edit Article")
     expect(page).not_to have_link("Delete Article")
+  end
+  
+  scenario "article sees both Edit and Delete buttons" do
+    login_as(@Steve)
+    
+    visit "/"
+    click_link @article.title
+    
+    expect(page).to have_content(@article.title)
+    expect(page).to have_content(@article.body)
+    expect(current_path).to eq(article_path(@article))
+    
+    expect(page).to have_link("Edit Article")
+    expect(page).to have_link("Delete Article")
   end
   
 end
